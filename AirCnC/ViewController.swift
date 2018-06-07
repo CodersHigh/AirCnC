52c495ce73fd5db1442a6a8c415b1b1cf66ad675
wannabewize <wannabewize@gmail.com>
2018-06-07 23:33:56 +0900

    씬과 뷰 예제, 스크린 샷, README

--- AirCnC/ViewController.swift
+++ AirCnC/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var depthLabel: UILabel!
+    @IBOutlet weak var widthLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        imageView.image = UIImage.init(named: "hattefjall")
+        
+        titleLabel.text = "HATTEFJÄLL"
+        
+        userImageView.image = #imageLiteral(resourceName: "franky")
+        userNameLabel.text = "프랭키"
+        
+        priceLabel.text = "10,000"
+        depthLabel.text = "68cm"
+        widthLabel.text = "68cm"
+        heightLabel.text = "110cm"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
