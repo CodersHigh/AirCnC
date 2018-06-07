200333dac9598450f28e3b6ba9906abfdf4fba5c
wannabewize <wannabewize@gmail.com>
2018-06-08 06:29:26 +0900

    날짜 선택 씬에 주석 추가

--- AirCnC/DateSelectViewController.swift
+++ AirCnC/DateSelectViewController.swift
@@ -21,6 +21,7 @@ class DateSelectViewController: UIViewController {
     weak var delegate: ReserveDateSelectDelegate?
     
     @IBAction func handleCancel(_ sender: Any) {
+        // 델리게이트 존재 확인, 옵셔널 함수 존재 확인
         if let delegate = delegate,
             let cancelFn = delegate.reserveDateSelectCancelled {
             cancelFn()
@@ -29,6 +30,7 @@ class DateSelectViewController: UIViewController {
     }
     
     @IBAction func handleDone(_ sender: Any) {
+        // 델리게이트가 존재하면 사용자가 선택한 값을 전달
         if let delegate = delegate {
             delegate.reserveDateDidSelected(from: startDatePicker.date, to: endDatePIcker.date)
         }
