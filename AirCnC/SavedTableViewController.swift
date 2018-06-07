407a31ad3cd1008a63e1fc0793510abf970f5807
wannabewize <wannabewize@gmail.com>
2018-06-08 05:39:15 +0900

    뭔가 이상한 부분 수정

--- AirCnC/SavedTableViewController.swift
+++ AirCnC/SavedTableViewController.swift
@@ -9,43 +9,39 @@
 import UIKit
 
 class SavedTableViewController: UITableViewController {
-    
-    var appDelegate: AppDelegate!
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        appDelegate = UIApplication.shared.delegate as! AppDelegate
         self.tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        appDelegate.removeSave( appDelegate.saves[indexPath.row] )
+        Liked.shared.saves.remove(at: indexPath.row)
         tableView.deleteRows(at: [indexPath], with: .automatic)
     }
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return appDelegate.saves.count
+        return Liked.shared.saves.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SavedCell", for: indexPath)
         
-        let item = appDelegate.saves[indexPath.row]
+        let item = Liked.shared.saves[indexPath.row]
         
-        cell.textLabel?.text = item.itemName
-        cell.imageView?.image = UIImage(named: item.itemImage)
-
+        cell.textLabel?.text = item.name
+        cell.imageView?.image = UIImage(named: item.thumbnail)
+        
         return cell
     }
 }
